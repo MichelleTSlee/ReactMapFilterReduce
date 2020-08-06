c077cb5d780a250f0d29f6be6643e8d02da249d9
Michelle Slee <sleemichelle@gmail.com>
2020-08-06 01:07:19 +0100

    Added 5 examples to index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,7 @@
-import React from 'react';
-import Card from './Card';
-import contacts from '../contacts';
-
-function createCard(contact) {
-  return <Card
-  key = {contact.id}
-  name={contact.name}
-  img = "https://picsum.photos/200"
-  email = {contact.email}
-  tel = {contact.tel}
-  />
-}
+import React from "react";
 
 function App() {
-  return (
-    <div className="flex-container">
-    {contacts.map(createCard)};
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles.css";
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-);
+let numbers = [2, 4, 6];
+
+//Map - creates a new array by doing something to each item in array
+let doubledNumber = numbers.map((num) => num * 2);
+console.log(doubledNumber);
+
+//Filter - checks each item in an array to see if it meets a condition
+let lessThanNumber = numbers.filter((num) => num < 6);
+console.log(lessThanNumber);
+
+//Reduce - Reduces array to one value by applying accumulator to item. Result becomes new accumulator - continues until one value is returned
+let totalNumbers = numbers.reduce((accumulator, num) => accumulator + num);
+console.log(totalNumbers);
+
+//Find - returns first value that matches the condition
+let firstMatchNum = numbers.find((num) => num > 2);
+console.log(firstMatchNum);
+
+//Index - returns index of first value that matches the condition
+let firstIndex = numbers.findIndex((num) => num > 2);
+console.log(firstIndex);
+
+ReactDOM.render(<App />, document.getElementById("root"));
